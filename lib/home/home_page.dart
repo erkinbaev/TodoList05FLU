9683f0cdabc44b78caabc05292151ad078fdb767
Nursultan <nursultan@MacBook-Air-Nursultan.local>
2026-05-16 18:37:42 +0600

    add settings page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list_05flu/add/add_page.dart';
 import 'package:todo_list_05flu/database/todo.dart';
+import 'package:todo_list_05flu/settings/settings_page.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -37,6 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [IconButton(onPressed: _navigateToSettingsPage, icon: Icon(Icons.settings))],
       ),
       body: Center(
         child: ListView.builder(
@@ -70,23 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
    } 
   }
 
-  void _onTapLike() {
-    setState(() {
-      // isLiked = !isLiked;
-      // if (isLiked == true) {
-      //     likes += 1;
-      // } else {
-      //     likes -= 1;
-      // }
-    });
-    
-    // print("likes: $likes");
-    // print("isLiked: $isLiked"); 
-  }
-
-  void _onCommentsTap() {
-    setState(() {
-     // isCommentsVisible = !isCommentsVisible;
-    });
+  void _navigateToSettingsPage() async {
+   Navigator.push(context, MaterialPageRoute(builder: (_) => SettingsPage()));
   }
 }
